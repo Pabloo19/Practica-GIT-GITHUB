3693f298e87941080b883791ab0a5f55156d400c
Pablo Escribano Fernandez <Pabloef34@educastur.es>
2024-12-04 18:25:17 +0100

    Archivo editado

--- Prueba.java
+++ Prueba.java
@@ -4,7 +4,8 @@ public class Prueba {
 		public static void main(String[] args) {
 			// Aqui empieza mi programa
 			System.out.println("Hola Mundo\n");
-			System.out.println("Esto es una prueba\n");		
+			System.out.println("Esto es una prueba\n");	
+			System.out.println("Esta linea es para ver la actualizacion de archivos\n");		
 			//Esta tarea esta sin terminar
 		}
 	}
