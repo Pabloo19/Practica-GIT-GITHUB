f83c48a63edbd9a8ea8f61d507930e10db849999
Pablo Escribano Fernandez <Pabloef34@educastur.es>
2024-12-04 20:08:21 +0100

    Archivo editado

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,6 +3,6 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// Aqui empieza mi programa
 		System.out.println("Hola Mundo");
-		//Esta tarea esta sin terminar
+		System.out.println("Hola Mundo esta es una practica con GIT y GITHUB");
 	}
 }
