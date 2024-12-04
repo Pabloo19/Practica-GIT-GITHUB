8c96cb89834cf5154b29f9ac19971d0ca41b06d7
Pablo Escribano Fernandez <Pabloef34@educastur.es>
2024-12-04 18:00:51 +0100

    Estos son mis ejemplos en el repositorio

---
+++ Prueba.java
@@ -0,0 +1,10 @@
+
+public class Prueba {
+	
+		public static void main(String[] args) {
+			// Aqui empieza mi programa
+			System.out.println("Hola Mundo\n");
+			System.out.println("Esto es una prueba\n");		
+			//Esta tarea esta sin terminar
+		}
+	}
